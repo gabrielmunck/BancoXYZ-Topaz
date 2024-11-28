7932c18e06e5f6d23cc47f8d09e1d8aab317b0be
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2024-11-28 16:06:41 -0300

    feat(app): criado componente de saldo do usuario e adicionado svg no bg

---
+++ src/components/AccountInfo.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useBanco } from "../contexts/BancoContext";
+import { IoEye, IoEyeOff } from "react-icons/io5";
+
+const AccountInfo = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+        fetchAccountInfo();
+    };
+    const { accountInfo, fetchAccountInfo } = useBanco();
+
+    return (
+        <div className="w-full">
+            <div className="flex flex-col items-center justify-center w-full h-auto">
+                <div className="mt-5 bg-background-darker w-full lg:max-w-[50%] p-5 rounded-lg">
+                    <span className="text-lg text-secondary font-bold">
+                        Seu saldo:{" "}
+                    </span>
+                    <div className="flex justify-between items-center my-2.5">
+                        <span className="text-2xl text-text-light font-bold mr-2.5">
+                            {isVisible
+                                ? `${accountInfo.currency}: ${parseFloat(accountInfo.accountBalance.toString()).
+                                toLocaleString('pt-BR', {minimumFractionDigits: 2,maximumFractionDigits: 2,})}`
+                                : "********"}
+                        </span>
+                        <button
+                            onClick={toggleVisibility}
+                            className="bg-background-dark rounded-full p-2"
+                            data-testid="toggle-visibility"
+                        >
+                            {isVisible ? (
+                                <IoEyeOff
+                                    size={24}
+                                    className="text-text-light"
+                                />
+                            ) : (
+                                <IoEye size={24} className="text-text-light" />
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AccountInfo;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,65 +1,44 @@
 import { useBanco } from "../contexts/BancoContext";
-import { useState } from "react";
 import Header from "../components/Header";
-import { IoEye, IoEyeOff } from "react-icons/io5";
+import AccountInfo from "../components/AccountInfo";
+
 
 const Home = () => {
-    const { signOut, user, accountInfo, fetchAccountInfo  } = useBanco();
-    const [isVisible, setIsVisible] = useState(false);
+    const { signOut, user  } = useBanco();
 
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible);
-        fetchAccountInfo();
-    };
+
+
     return (
         <div className="flex flex-1 items-center justify-center w-full bg-background-darker">
             <div className="flex flex-1 w-[95%] h-screen bg-background-dark">
-                <div className="flex flex-col w-full">
+                <div className="absolute top-10 left-0 w-full h-screen overflow-hidden">
+                    <svg
+                        className="absolute opacity-5 -left-[200px] h-screen overflow-visible"
+                        viewBox="0 0 256 512"
+                        fill="white"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M209.2 233.4l-108-31.6C88.7 198.2 80 186.5 80 173.5c0-16.3 13.2-29.5 29.5-29.5h66.3c12.2 0 24.2 3.7 34.2 10.5 6.1 4.1 14.3 3.1 19.5-2l34.8-34c7.1-6.9 6.1-18.4-1.8-24.5C238 74.8 207.4 64.1 176 64V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48h-2.5C45.8 64-5.4 118.7.5 183.6c4.2 46.1 39.4 83.6 83.8 96.6l102.5 30c12.5 3.7 21.2 15.3 21.2 28.3 0 16.3-13.2 29.5-29.5 29.5h-66.3C100 368 88 364.3 78 357.5c-6.1-4.1-14.3-3.1-19.5 2l-34.8 34c-7.1 6.9-6.1 18.4 1.8 24.5 24.5 19.2 55.1 29.9 86.5 30v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-48.2c46.6-.9 90.3-28.6 105.7-72.7 21.5-61.6-14.6-124.8-72.5-141.7z"/>
+                    </svg>
+                </div>
+                <div className="flex flex-col w-full z-50">
                     <Header/>
                     <div className="px-10 pt-20">
-                        <h2 className="text-text-light text-center lg:text-left lg:pl-20 text-4xl mb-10">
+                        <h2 className="text-text-light text-center text-4xl font-bold mb-10" style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)" }}>
                             Olá, {user.name}!
                         </h2>
-
                         <div className="flex flex-col items-center justify-center w-full h-auto">
-                            <div className="mt-5 bg-background-darker w-full lg:max-w-[50%] p-5 rounded-lg">
-                                <span className="text-lg text-secondary font-bold">
-                                    Seu saldo:{" "}
+                            <AccountInfo/>
+                        </div>
+                        <div className="pt-5 w-full flex">
+                            <button
+                                onClick={signOut}
+                                className="max-w-[50%] w-full mx-auto bg-primary hover:bg-primary-hover py-1.5 rounded"
+                            >
+                                <span className="text-text-light text-base font-bold text-center">
+                                    Sair
                                 </span>
-                                <div className="flex justify-between items-center my-2.5">
-                                    <span className="text-2xl text-text-light font-bold mr-2.5">
-                                        {isVisible
-                                            ? `${accountInfo.currency}: ${parseFloat(
-                                                accountInfo.accountBalance.toString()
-                                            ).toLocaleString('pt-BR', {
-                                                minimumFractionDigits: 2,
-                                                maximumFractionDigits: 2,
-                                            })}`
-                                            : "********"}
-                                    </span>
-                                    <button
-                                        onClick={toggleVisibility}
-                                        className="bg-background-dark rounded-full p-2"
-                                        data-testid="toggle-visibility"
-                                    >
-                                        {isVisible ? (
-                                            <IoEyeOff size={24} className="text-text-light" />
-                                        ) : (
-                                            <IoEye size={24} className="text-text-light" />
-                                        )}
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="mt-5 w-full flex">
-                                <button
-                                    onClick={signOut}
-                                    className="max-w-[50%] w-full mx-auto bg-primary hover:bg-primary-hover py-1.5 rounded"
-                                >
-                                    <span className="text-text-light text-base font-bold text-center">
-                                        Sair
-                                    </span>
-                                </button>
-                            </div>
+                            </button>
                         </div>
                     </div>
                 </div>
